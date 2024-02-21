be1d419a31767a7bea13ef2f24861ff2a0f47f36
Ben Golding <timbotenbo99@gmail.com>
2024-02-21 15:14:40 +0000

    Basic contacts page populating from store, no way to add contact yet

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
+from .models import Contacts
 
 main = Blueprint('main', __name__)
 
@@ -12,3 +13,10 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name, isAdmin=(current_user.userType == "Admin"))
+
+@main.route('/contacts')
+@login_required
+def contacts():
+    contacts = Contacts.query.all()
+    contactsCount = len(contacts)
+    return render_template('contacts.html', isAdmin=(current_user.userType == "Admin"), contacts=contacts, contactsCount=contactsCount)
--- project/models.py
+++ project/models.py
@@ -2,8 +2,15 @@ from . import db
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    name = db.Column(db.String(100))
     userType = db.Column(db.String(100))
+
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    company = db.Column(db.String(100))
+    phoneNumber = db.Column(db.Integer)
+    email = db.Column(db.String(100))
