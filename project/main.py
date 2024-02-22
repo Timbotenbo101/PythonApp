ba7f650443bfb30cfa9da6c751e1d4a66a41d8f5
Ben Golding <timbotenbo99@gmail.com>
2024-02-22 14:55:57 +0000

    Add contact page

--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, flash, url_for
 from flask_login import login_required, current_user
 from . import db
 from .models import Contacts
@@ -20,3 +20,30 @@ def contacts():
     contacts = Contacts.query.all()
     contactsCount = len(contacts)
     return render_template('contacts.html', isAdmin=(current_user.userType == "Admin"), contacts=contacts, contactsCount=contactsCount)
+
+@main.route('/addcontact')
+@login_required
+def addcontact():
+    return render_template('addContact.html')
+
+@main.route('/addcontact', methods=['POST'])
+@login_required
+def addcontact_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    company = request.form.get('company')
+    telephone = request.form.get('telephone')
+
+    contact = Contacts.query.filter_by(email=email).first() # if this returns a contact, then the email already exists in database
+
+    if contact: # if a user is found, we want to redirect back to signup page so user can try again
+        flash('A contact with this email address already exists')
+        return redirect(url_for('main.addcontact'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_contact = Contacts(email=email, name=name, company=company, phoneNumber=telephone)
+
+    # add the new user to the database
+    db.session.add(new_contact)
+    db.session.commit()
+    return redirect(url_for('main.contacts'))
