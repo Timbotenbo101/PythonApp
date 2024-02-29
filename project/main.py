dc6c274b8fe54c457a8c4ccdd48f39fba3ea0300
Ben Golding <timbotenbo99@gmail.com>
2024-02-29 15:34:45 +0000

    Validation on contact forms, implement closable notifications

--- project/main.py
+++ project/main.py
@@ -1,3 +1,4 @@
+import re
 from flask import Blueprint, redirect, render_template, request, flash, url_for
 from flask_login import login_required, current_user
 from . import db
@@ -34,18 +35,26 @@ def addcontact_post():
     company = request.form.get('company')
     telephone = request.form.get('telephone')
 
+    errors = validateContact(name,email,company,telephone)
+
     contact = Contacts.query.filter_by(email=email).first() # if this returns a contact, then the email already exists in database
 
     if contact: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('A contact with this email address already exists')
+        errors.append('A contact with this email address already exists')
+    
+    if len(errors) > 0:
+        for error in errors:
+            # flash all validation messages ready for display when returning to form
+            flash(error)
         return redirect(url_for('main.addcontact'))
-
+    
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_contact = Contacts(email=email, name=name, company=company, phoneNumber=telephone)
 
     # add the new user to the database
     db.session.add(new_contact)
     db.session.commit()
+    flash('Successfully added contact')
     return redirect(url_for('main.contacts'))
 
 @main.route('/editcontact/<int:contact_id>')
@@ -64,11 +73,19 @@ def editcontact_post():
     telephone = request.form.get('telephone')
 
     contact = Contacts.query.filter_by(id = id).first()
+    
+    errors = validateContact(name,email,company,telephone)
+
     duplicateContact = Contacts.query.filter(id != id, email == email).first() # if this returns a contact, then the email already exists in database
 
     if duplicateContact: # redirect back if there is a separate contact with matching email address
-        flash('Another contact with this email address already exists')
-        return redirect(url_for('main.addcontact'))
+        errors.append('Another contact with this email address already exists')
+    
+    if len(errors) > 0:
+        for error in errors:
+            # flash all validation messages ready for display when returning to form
+            flash(error)
+        return render_template('editContact.html', contact=contact)
 
     # update contact data and commit
     contact.email = email
@@ -76,6 +93,7 @@ def editcontact_post():
     contact.company = company
     contact.phoneNumber = telephone
     db.session.commit()
+    flash('Successfully updated contact')
     return redirect(url_for('main.contacts'))
 
 @main.route('/deletecontact/<int:contact_id>')
@@ -90,5 +108,31 @@ def confirmdelete(contact_id):
     contact = Contacts.query.filter_by(id=contact_id).first()
     db.session.delete(contact)
     db.session.commit()
+    flash('Successfully deleted contact')
     return redirect(url_for('main.contacts'))
+
+def validateContact(name, email, company, telephone):
+    errors = []
+    # Server side validation to protect against users editing the html before submission
+    if (len(str.strip(email)) == 0): 
+        errors.append('Email is a required field')
+    else:
+        email_validate_pattern = r"^\S+@\S+\.\S+$"
+        if not re.match(email_validate_pattern, email):
+            errors.append("Please enter a valid email address")
+            
+    if (len(str.strip(name)) == 0):
+        errors.append('Name is a required field')
+    if (len(str.strip(company)) == 0): 
+        errors.append('Company is a required field')
+
+    if (len(str.strip(telephone)) == 0): 
+        errors.append('Telephone is a required field')
+    else:
+        validNumber = telephone.isnumeric()
+        if (validNumber == False):
+            errors.append('Phone number must not contain letters or special characters')
+
+    return errors
+    
     
