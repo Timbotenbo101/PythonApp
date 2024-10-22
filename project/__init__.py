10d3065ebf8c815858d1a9eaa617a43a21da4776
Ben Golding <timbotenbo99@gmail.com>
2024-10-22 10:41:22 +0100

    Changes to __init__

--- project/__init__.py
+++ project/__init__.py
@@ -3,42 +3,50 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from werkzeug.security import generate_password_hash
 
+
+
 db = SQLAlchemy()
 
-app = Flask(__name__)
+def create_app():
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
-app.config['SECRET_KEY'] = 'contactsflaskapplication123'
+    app = Flask(__name__)
 
-db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+    app.config['SECRET_KEY'] = 'contactsflaskapplication123'
 
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
+    db.init_app(app)
 
-from .models import User
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    from .models import User
 
-from . import models
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
-from .auth import auth as auth_blueprint
-app.register_blueprint(auth_blueprint)
+    from . import models
 
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
-with app.app_context():
-    db.create_all()
-    if not User.query.filter(User.email == 'admin@admin.com').first():
-        user = User(
-            email='admin@admin.com',
-            password=generate_password_hash('Admin123'),
-            name='Admin User',
-            userType='Admin'
-        )
-        db.session.add(user)
-        db.session.commit()
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
+    with app.app_context():
+        db.create_all()
+        if not User.query.filter(User.email == 'admin@admin.com').first():
+            user = User(
+                email='admin@admin.com',
+                password=generate_password_hash('Admin123'),
+                name='Admin User',
+                userType='Admin'
+            )
+            db.session.add(user)
+            db.session.commit()
+
+    return app
+
+
+create_app()
